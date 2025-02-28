1fba0b0dfdeaf9799597d353bd0bf1f292432e66
Samuel <samuelsegala08@gmail.com>
2025-02-28 10:34:14 -0300

    atividade

---
+++ revisao/Samuel_Yasmin/helloworld.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+        <title>Hello World</title>
+        <?php
+                echo "Hello World!";
+                echo 'Samuel e Yasmin'
+        ?>
+</head>
+<body>
+        
+</body>
+</html>
